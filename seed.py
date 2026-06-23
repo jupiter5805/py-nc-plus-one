96bed0a5443c20ddff09ac3014f04d1ce304cdfb
Muhammad Usman <muhammadusman5805@gmail.com>
2026-06-23 17:30:41 +0100

    Seed venues table

--- seed.py
+++ seed.py
@@ -7,6 +7,7 @@ def seed():
     cursor = connection.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS users;")
+    cursor.execute("DROP TABLE IF EXISTS venues;")
 
     cursor.execute("""
         CREATE TABLE users (
@@ -14,6 +15,15 @@ def seed():
             name VARCHAR NOT NULL,
             email VARCHAR NOT NULL,
             password VARCHAR NOT NULL
+        );
+    """)
+
+    cursor.execute("""
+        CREATE TABLE venues (
+            venue_id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            address VARCHAR,
+            capacity INT NOT NULL
         );
     """)
 
@@ -27,6 +37,17 @@ def seed():
             VALUES
             (%s, %s, %s);
         """, [user["name"], user["email"], user["password"]])
+
+    with open("db/data/venues.json", "r") as file:
+        venues = json.load(file)
+
+    for venue in venues:
+        cursor.execute("""
+            INSERT INTO venues
+            (name, address, capacity)
+            VALUES
+            (%s, %s, %s);
+        """, [venue["name"], venue["address"], venue["capacity"]])
 
     connection.commit()
     cursor.close()
